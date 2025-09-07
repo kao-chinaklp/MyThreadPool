14a7ef84f67ad793effa7e359c7d358a87795801
kao-chinaklp <kao-chinaklp@outlook.com>
2025-09-07 22:54:40 +0800

    Fixed serious bugs from Queue.h.

--- header/MyThreadPool.h
+++ header/MyThreadPool.h
@@ -8,59 +8,63 @@
 #include "Vector.h"
 
 class MyThreadPool {
-public:
-    struct Job {
-        void (*Function)(void *); // 任务函数
-        void* Data;
-    };
+    public:
+        struct Job {
+            void (*Function)(void *); // 任务函数
+            void* Data;
+        };
 
-    struct Worker {
-        Worker():Terminate(false), isWorking(false), Pool(nullptr) {}
-        pthread_t ThreadID{};
-        bool Terminate; // 判断线程是否终止
-        bool isWorking; // 判断线程是否在工作
-        MyThreadPool *Pool;
-    };
+        struct Worker {
+            Worker():Terminate(false), Pool(nullptr) {}
+            pthread_t ThreadID{};
+            bool Terminate; // 判断线程是否终止
+            MyThreadPool *Pool;
+        };
 
-public:
-    MyThreadPool()=default;
-    MyThreadPool(ui maxWorker, ui maxJob);
-    ~MyThreadPool();
+    public:
+        MyThreadPool()=default;
+        explicit MyThreadPool(ui maxWorker);
+        ~MyThreadPool();
 
-    void AddThreads(ui count); // 添加线程
+    public:
+        void* CacheProcess(void* data); // 控制线程池运行
 
-public:
-    void StopAll(); // 停止所有线程
+        void StopAll(); // 停止所有线程
 
-    static void* Run(void* data); // 执行任务
+        static void* Run(void* data); // 执行任务
 
-    void* ThreadLoop(void* data); // 线程循环
+        void* ThreadLoop(void* data); // 线程循环
 
-    void PushJob(void (*Function)(void*), void* Data);
+        void PushJob(void (*Function)(void*), void* Data);
 
-protected:
-    struct Locker {
-        explicit Locker(pthread_mutex_t *mutex):mutex(mutex) {
-            ui attempt=1;
-            constexpr ui maxAttempts=1000;
-            while(pthread_mutex_trylock(mutex)!=0&&attempt<maxAttempts)
-                std::this_thread::sleep_for(std::chrono::microseconds(attempt<<=1)); // 等待锁
-            if (attempt>=maxAttempts)std::this_thread::yield(); // 极端情况
-        }
-        ~Locker() {pthread_mutex_unlock(mutex);}
+    protected:
+        struct Locker {
+            explicit Locker(pthread_mutex_t *mutex):mutex(mutex) {
+                ui attempt=1;
+                constexpr ui maxAttempts=1000;
+                while(pthread_mutex_trylock(mutex)!=0&&attempt<maxAttempts)
+                    std::this_thread::sleep_for(std::chrono::microseconds(attempt<<=1)); // 等待锁
+                if (attempt>=maxAttempts) locked=false; // 极端情况
+                else locked=true;
+            }
+            ~Locker() {pthread_mutex_unlock(mutex);}
 
-        pthread_mutex_t *mutex;
-    };
+            bool locked;
+            pthread_mutex_t *mutex;
+        };
 
-private:
-    ui maxWorker{};
-    ui maxJob{};
-    ui freeWorker{}; // 空闲线程数
-    Queue<Job*> taskList; // 任务队列
-    Vector<Worker> workers;
-    pthread_mutex_t mutex{};
-    pthread_cond_t cond{};
-    pthread_cond_t mainCond{}; // 用于控制主线程等待
+    private:
+        ui maxWorker{};
+        ui freeWorker{}; // 空闲线程数
+        bool terminate{};
+
+        Queue<Job*> taskList; // 任务队列
+        Vector<Worker> workers;
+        pthread_t controlThreadID{};
+        pthread_mutex_t mutex{};
+        pthread_mutex_t counterMutex{};
+        pthread_cond_t cond{};
+        pthread_cond_t cacheCond{}; // 用于控制线程池运行
 };
 
 #endif //MYTHREADPOOL_H
--- src/MyThreadPool.cpp
+++ src/MyThreadPool.cpp
@@ -2,25 +2,22 @@
 
 #include <mutex>
 
-MyThreadPool::MyThreadPool(const ui maxWorker, const ui maxJob): maxWorker(maxWorker), maxJob(maxJob) {
-    if(maxWorker<1||maxJob<1)return; // 线程数和任务数必须大于0
+MyThreadPool::MyThreadPool(const ui maxWorker): maxWorker(maxWorker) {
+    if(maxWorker<1)return; // 线程数和任务数必须大于0
     if(pthread_cond_init(&cond, nullptr)!=0)return; // 初始化条件变量
-    if(pthread_cond_init(&mainCond, nullptr)!=0)return; // 初始化条件变量
+    if(pthread_cond_init(&cacheCond, nullptr)!=0)return;
     if(pthread_mutex_init(&mutex, nullptr)!=0)return; // 初始化互斥锁
+    if(pthread_mutex_init(&counterMutex, nullptr)!=0)return;
 
     // 初始化worker
-    workers.Resize(maxWorker);
-    for(Worker& i:workers) {
-        i.Pool=this;
-        if(pthread_create(&i.ThreadID, nullptr, Run, &i)!=0) {
+    workers.Resize(maxWorker+1);
+    for(ui i=0;i<=maxWorker;++i) {
+        workers[i].Pool=this;
+        if(pthread_create(&workers[i].ThreadID, nullptr, Run, &workers[i])!=0) {
             workers.Clear();
             return; // 创建线程失败
         }
-        if(pthread_detach(i.ThreadID)) {
-            workers.Clear();
-            return; // 分离线程失败
-        }
-        i.Terminate=false;
+        workers[i].Terminate=false;
     }
 
     freeWorker=maxWorker;
@@ -30,76 +27,63 @@ MyThreadPool::~MyThreadPool() {
     StopAll();
     workers.Clear();
     pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&cacheCond);
     pthread_mutex_destroy(&mutex);
 }
 
-void MyThreadPool::AddThreads(ui count) {
-    if(count==0)return;
-    const ui oldSize=workers.Size();
-    workers.Resize(oldSize+count);
-
-    for(ui i=oldSize;i<workers.Size();++i) {
-        workers[i].Pool=this;
-        if(const int ret=pthread_create(&workers[i].ThreadID, nullptr, Run, &workers[i]);ret!=0) {
-            workers.Resize(oldSize);
-            return; // 创建线程失败
+void* MyThreadPool::CacheProcess(void* data) {
+    while (true) {
+        Locker locker(&mutex);
+        while (taskList.Empty()) {
+            if (terminate) return nullptr;
+            pthread_cond_wait(&cacheCond, &mutex);
         }
-        if(pthread_detach(workers[i].ThreadID)) {
-            workers.Resize(oldSize);
-            return; // 分离线程失败
-        }
-        workers[i].Terminate=false;
+        while (freeWorker==0) pthread_cond_wait(&cacheCond, &mutex);
+        pthread_cond_signal(&cond); // 唤醒一个工作线程
     }
-
-    Locker locker(&mutex);
-    freeWorker+=count;
 }
 
 void MyThreadPool::StopAll() {
-    for(Worker& i:workers)
-        if (!i.Terminate) i.Terminate=true;
-    pthread_cond_broadcast(&cond); // 唤醒所有线程
+    terminate=true;
+    pthread_cond_signal(&cacheCond); // 处理没处理完的任务
 
-    while (!taskList.Empty()) pthread_cond_wait(&mainCond, &mutex);
     for (const Worker& i:workers)
-        while (i.isWorking) pthread_cond_wait(&mainCond, &mutex);
+        pthread_join(i.ThreadID, nullptr);
 }
 
 void* MyThreadPool::Run(void* data) {
     const auto* worker=static_cast<Worker*>(data);
+
     if(worker==nullptr||worker->Pool==nullptr)return nullptr;
-    worker->Pool->ThreadLoop(data);
-    return nullptr;
+    if (worker->ThreadID==worker->Pool->workers[0].ThreadID)
+        return worker->Pool->CacheProcess(data);
+    return worker->Pool->ThreadLoop(data);
 }
 
 void* MyThreadPool::ThreadLoop(void* data) {
-    auto* worker=static_cast<Worker*>(data);
     while(true) {
-        const auto* locker=new Locker(&mutex);
-        while(taskList.Size()==0) {
-            if(worker->Terminate) {
-                delete locker;
-                pthread_cond_signal(&mainCond); // 唤醒主线程
-                return nullptr;
-            }
-            pthread_cond_wait(&cond, &mutex);
-        }
+        auto* locker=new Locker(&mutex);
+        if (!locker->locked) continue;
+        pthread_cond_wait(&cond, &mutex);
 
+        auto* counterLocker=new Locker(&counterMutex);
         --freeWorker;
-        auto job=taskList.Front();
-        if(job!=nullptr) {
-            taskList.Pop();
-            delete locker;
+        delete counterLocker;
 
-            worker->isWorking=true;
+        auto job=taskList.Front();
+        taskList.Pop();
+        delete locker;
+        if(job!=nullptr) {
             job->Function(job->Data);
-            worker->isWorking=false;
 
             free(job->Data);
             free(job);
         }
-        else delete locker;
+        counterLocker=new Locker(&counterMutex);
         ++freeWorker;
+        delete counterLocker;
+
+        pthread_cond_signal(&cacheCond);
     }
 }
 
@@ -110,15 +94,15 @@ void MyThreadPool::PushJob(void (*Function)(void*), void* Data) {
     job->Function=Function;
     job->Data=Data;
 
-    const auto* locker=new Locker(&mutex);
-    if(taskList.Size()>=maxJob) {
-        free(job->Data);
-        free(job);
+    auto* locker=new Locker(&mutex);
+    while (!locker->locked) {
+        std::this_thread::sleep_for(std::chrono::microseconds(100));
         delete locker;
-        return;
+        locker=new Locker(&mutex);
     }
 
     taskList.Push(job);
     delete locker;
-    pthread_cond_signal(&cond); // 唤醒线程
+    if (freeWorker>0) pthread_cond_signal(&cond);
+    else pthread_cond_signal(&cacheCond);
 }
